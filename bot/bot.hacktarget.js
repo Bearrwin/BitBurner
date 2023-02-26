b19e640530929e411cf5c72b860c0f68e03e51a0
Bearrwin <125781212+Bearrwin@users.noreply.github.com>
2023-02-27 09:28:53 +1100

    Add files via upload

--- bot/bot.hacktarget.js
+++ bot/bot.hacktarget.js
@@ -2,11 +2,11 @@
 export async function main(ns) {
 
 
-	ns.disableLog("sleep")
-	ns.disableLog("getHackingLevel")
-	ns.disableLog("getServerMoneyAvailable")
-	ns.disableLog("getServerRequiredHackingLevel")
-	ns.disableLog("getServerMaxRam")
+	// ns.disableLog("sleep")
+	// ns.disableLog("getHackingLevel")
+	// ns.disableLog("getServerMoneyAvailable")
+	// ns.disableLog("getServerRequiredHackingLevel")
+	// ns.disableLog("getServerMaxRam")
 	ns.tail()
 	// n00dles is the first target to hit because it is the best growth rate until you outgrow it.
 
@@ -40,9 +40,9 @@ export async function main(ns) {
 			ns.run("/utils/isexists.purchServ.js")
 			await ns.sleep(1000)
 			servCount = ns.read("/savedVar/purchServCount.txt")
-			if (servCount > 0) {
+			while (wgh == true) {
+				if (servCount > 0) {
 
-				while (wgh == true) {
 					if (ns.fileExists("BruteSSH.exe", "home") && (ns.getHackingLevel() > 150)) {
 						if (stageOnephaseTwoDone == false) {
 							currentTargetOne = secondTarget
@@ -92,49 +92,52 @@ export async function main(ns) {
 						}
 						newTarget = false
 					}
+					await ns.sleep(11)
+
 				}
-			}
 
 
 
 
-			//	run the worm to crawl the network and use any available crack 
-			//	programs and nuke.exe on any server with enough ports open.
-			ns.run("/worm/worm.nuke.js")
-			ns.tprint("Running Nuke worm to crack all possible ports and nuke all possible servers.")
-			ns.tprint("")
-			await ns.sleep(30000)
-			// 	run the worm that kills all scripts on all servers except home.
-			ns.run("/worm/worm.killall.excepthome.js")
-			ns.tprint("Killing old scripts everywhere but home")
-			ns.tprint("")
-			await ns.sleep(5000)
-			// run the worm that copies wgh script to npc servers and runs them at our current target.
-			// we always want these servers hitting n00dles their firepower is too low to bother
-			// with anything else
-			ns.run("/worm/worm.wgh.nocrack.loop.npconly.js", 1, firstTarget)
-			ns.tprint("Running combo wgh worm on all NPC servers to target " + firstTarget)
-			ns.tprint("")
-			await ns.sleep(30000)
-			// run the worm that copies wgh script to npc servers and runs them at our current target.
-			if (servCount > 0) {
-				ns.run("/worm/worm.wgh.nocrack.loop.pconly.3.js", 1, currentTargetOne, currentTargetTwo, currentTargetThree,)
-				ns.tprint("Running combo wgh worm on all PC servers (if we have any)")
+				//	run the worm to crawl the network and use any available crack 
+				//	programs and nuke.exe on any server with enough ports open.
+				ns.run("/worm/worm.nuke.js")
+				ns.tprint("Running Nuke worm to crack all possible ports and nuke all possible servers.")
 				ns.tprint("")
-			}
+				await ns.sleep(30000)
+				// 	run the worm that kills all scripts on all servers except home.
+				ns.run("/worm/worm.killall.excepthome.js")
+				ns.tprint("Killing old scripts everywhere but home")
+				ns.tprint("")
+				await ns.sleep(5000)
+				// run the worm that copies wgh script to npc servers and runs them at our current target.
+				// we always want these servers hitting n00dles their firepower is too low to bother
+				// with anything else
+				ns.run("/worm/worm.wgh.nocrack.loop.npconly.js", 1, firstTarget)
+				ns.tprint("Running combo wgh worm on all NPC servers to target " + firstTarget)
+				ns.tprint("")
+				await ns.sleep(30000)
+				// run the worm that copies wgh script to npc servers and runs them at our current target.
+				if (servCount > 0) {
+					ns.run("/worm/worm.wgh.nocrack.loop.pconly.3.js", 1, currentTargetOne, currentTargetTwo, currentTargetThree,)
+					ns.tprint("Running combo wgh worm on all PC servers (if we have any)")
+					ns.tprint("")
+				}
 
-			while (newTarget == false) {
+				while (newTarget == false) {
 
+					newTarget = ns.read("/savedVar/newTarget.txt") === "true" ? true : false;
+					await ns.sleep(6000)
+
+
+				}
+				ns.tprint("We have a new targeting request")
+				ns.write("/savedVar/newTarget.txt", "false", "w")
+				await ns.sleep(1000)
 				newTarget = ns.read("/savedVar/newTarget.txt") === "true" ? true : false;
-				await ns.sleep(6000)
-
-
 			}
-			ns.tprint("We have a new targeting request")
-			ns.write("/savedVar/newTarget.txt", "false", "w")
-			await ns.sleep(1000)
-			newTarget = ns.read("/savedVar/newTarget.txt") === "true" ? true : false;
 		}
+		await ns.sleep(10)
 	}
 
 
