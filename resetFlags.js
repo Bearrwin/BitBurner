1f99c0ee6d16680336c4996bc33f53ef41e8a571
Bearrwin <125781212+Bearrwin@users.noreply.github.com>
2023-02-28 21:16:17 +1100

    Add files via upload

---
+++ resetFlags.js
@@ -0,0 +1,26 @@
+/** @param {NS} ns */
+export async function main(ns) {
+ns.write("/savedVar/bitrunnersDone.txt", "false", "w")
+ns.write("/savedVar/blackhandDone.txt", "false", "w")
+ns.write("/savedVar/chongqingDone.txt", "false", "w")
+ns.write("/savedVar/chonqingDone.txt", "false", "w")
+ns.write("/savedVar/csecDone.txt", "false", "w")
+ns.write("/savedVar/daedalusDone.txt", "false", "w")
+ns.write("/savedVar/newTarget.txt", "false", "w")
+ns.write("/savedVar/niteSecDone.txt", "false", "w")
+ns.write("/savedVar/stageEightDone.txt", "false", "w")
+ns.write("/savedVar/stageFiveDone.txt", "false", "w")
+ns.write("/savedVar/stageFourDone.txt", "false", "w")
+ns.write("/savedVar/stageOneDone.txt", "false", "w")
+ns.write("/savedVar/stageSevenDone.txt", "false", "w")
+ns.write("/savedVar/stageSixDone.txt", "false", "w")
+ns.write("/savedVar/stageThreeDone.txt", "false", "w")
+ns.write("/savedVar/stageTwoDone.txt", "false", "w")
+ns.write("/savedVar/stwelveDone.txt", "false", "w")
+ns.write("/savedVar/tetradsDone.txt", "false", "w")
+ns.write("/savedVar/tianDiHuiDone.txt", "false", "w")
+ns.write("/savedVar/stageOne.txt", "true", "w")
+ns.write("/savedVar/purchServCount.txt", "", "w")
+ns.write("/savedVar/purchServList.txt", "", "w")
+
+}
