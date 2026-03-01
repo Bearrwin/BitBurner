95b4514d33e78d2b1faae3cbaaeee5799e45c63d
Bearrwin <125781212+Bearrwin@users.noreply.github.com>
2026-03-02 04:15:32 +1100

    Delete serv directory

--- serv/serv.delete.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-	let server = ns.args[0];
-
-	// self-explanatory
-	ns.deleteServer(server)
-
-}
--- serv/serv.upg.js
+++
@@ -1,9 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-	var serverName = ns.args[0];
-	var ram = ns.args[1];
-
-	// self-explanatory
-	ns.upgradePurchasedServer(serverName, (ram));
-
-}
