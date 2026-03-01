d05735510a076a29b9b30224e344e50975c4e6bc
Bearrwin <125781212+Bearrwin@users.noreply.github.com>
2026-03-02 04:14:13 +1100

    Delete utils directory

--- utils/faction.price.augment.js
+++
@@ -1,9 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-	let augName = ns.args[0];
-	let augPrice = ns.singularity.getAugmentationBasePrice(augName)
-	ns.tprint(`${ns.nFormat(augPrice, "$0.000a")} `)
-	
-
-
-}
